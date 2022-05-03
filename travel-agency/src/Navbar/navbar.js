e40e424e4277aa8f4f71fedbd2815d666c4913ac
RellzCodez <adeyemiodunayo21@gmail.com>
2022-05-03 13:52:51 -0700

    added to the component

---
+++ travel-agency/src/Navbar/navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+function navbar() {
+  return (
+    <div className='navbar'>
+    <div>
+      Arctic Travels
+      
+    </div>
+
+    <ul>
+      <li>About Us</li>
+      <li>Support</li>
+      <li>Language</li>
+    </ul>
+
+    </div>
+  )
+}
+
+export default navbar
